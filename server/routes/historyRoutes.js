2a89559a357478f61e9fde0c23db995500d8c624
karthikthorlapati <karthikthorlapati123@gmail.com>
2026-07-20 23:12:26 +0530

    routed fixed

--- server/routes/historyRoutes.js
+++ server/routes/historyRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import historyController from "../controllers/historyController.js";
-import checkAuth from "../middleware/checkAuth.js";
+import checkAuth from "../middleware/auth.js";
 
 const router = express.Router();
 
